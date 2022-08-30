be3dba2713551ea02e43372a4978d39f3a505a34
Arthur Barbera <tartur.dev@gmail.com>
2022-08-31 00:34:18 +0200

    Clear

--- Parsing/HrmlParser.cpp
+++
@@ -1,7 +0,0 @@
-#include "HrmlParser.hpp"
-
-HrmlParser::HrmlParser(std::vector<std::string> hrml, std::vector<std::string> queries) noexcept
-{
-    /* TODO : if !(implemented (TagAttributes) ) then do it
-     * TODO : else then use it to correctly initialize all tags attributes */
-}
--- Parsing/TagAttributes.cpp
+++
@@ -1,6 +0,0 @@
-#include "TagAttributes.hpp"
-
-TagAttributes::TagAttributes(std::vector<std::string> hrml, std::vector<std::string> queries) noexcept
-{
-    // TODO : IMPLEMENT THIS SHIT
-}
